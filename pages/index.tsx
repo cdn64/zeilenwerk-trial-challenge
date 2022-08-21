4ba9bc10d965123ec78d1ee5c26ee02e0916a105
cdn64 <gitcdn@gmail.com>
2022-08-21 09:09:03 +0200

    feat: add loading and error handling to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,17 @@ export async function getServerSideProps() {
   return { props: { organizations, nextSince } }
 }
 
+enum FetchingState {
+  Idle,
+  Loading,
+  Failed,
+}
+
 const Home: NextPage<OrganizationsResponse> = ({
   organizations: initialOrganizations,
   nextSince: initialNextSince,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [fetchingState, setFetchingState] = useState<FetchingState>(FetchingState.Idle)
   const [data, setData] = useState({
     organizations: initialOrganizations,
     nextSince: initialNextSince,
@@ -20,20 +27,42 @@ const Home: NextPage<OrganizationsResponse> = ({
   const { organizations, nextSince } = data
 
   const fetchNewPage = async () => {
-    const response = await fetch(`http://localhost:3001/api/organizations?since=${nextSince}`)
-    const newData = await response.json()
+    setFetchingState(FetchingState.Loading)
+    try {
+      const response = await fetch(`http://localhost:3001/api/organizations?since=${nextSince}`)
+      const newData = await response.json()
 
-    setData((data) => ({
-      organizations: [...data.organizations, ...newData.organizations],
-      nextSince: newData.nextSince,
-    }))
+      setData((data) => ({
+        organizations: [...data.organizations, ...newData.organizations],
+        nextSince: newData.nextSince,
+      }))
+      setFetchingState(FetchingState.Idle)
+    } catch (error) {
+      setFetchingState(FetchingState.Failed)
+    }
+  }
+
+  const renderLoadMore = () => {
+    switch (fetchingState) {
+      case FetchingState.Idle:
+        return <button onClick={fetchNewPage}>Load more</button>
+      case FetchingState.Loading:
+        return <p>Loading additional organizations…</p>
+      case FetchingState.Failed:
+        return (
+          <>
+            <p>Failed to load additional organizations!</p>
+            <button onClick={fetchNewPage}>Try again</button>
+          </>
+        )
+    }
   }
 
   return (
     <div>
       <h1>GitHub organizations</h1>
       <OrganizationList organizations={organizations} />
-      <button onClick={fetchNewPage}>Load more</button>
+      {renderLoadMore()}
     </div>
   )
 }
